5f2e46ba29c0d94ea336b4d45dd3a22f20d10e40
Anurag <alexandersharma28@gmail.com>
2023-01-09 13:44:23 +0530

    Added Water for when thirsty

--- hungry.py
+++ hungry.py
@@ -4,4 +4,7 @@ if hungry == 'yes':
     print("Eat pizza")
     print("Eat burger")
 else:
+    thirsty = input("Are ypu thirsty: ")
+    if thirsty = "yes":
+        print("Drink Water")
     print("Do your Homework!")
